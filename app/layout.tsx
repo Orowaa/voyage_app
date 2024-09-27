cbe43e20bcdaf4f226df6f7f892f9b3a78858a0d
Serge Y <124675137+Orowaa@users.noreply.github.com>
2024-09-27 21:06:07 +0200

    Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 
 export const metadata: Metadata = {
@@ -16,7 +18,10 @@ export default function RootLayout({
     <html lang="fr">
       <body
       >
-        {children}
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main> 
+        <Footer />
+        
       </body>
     </html>
   );

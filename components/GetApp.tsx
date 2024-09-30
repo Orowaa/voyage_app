8bf309abead569e900834d845d9767ac88473078
Serge Y <124675137+Orowaa@users.noreply.github.com>
2024-09-30 13:58:21 +0200

    mobileApp

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -2,7 +2,19 @@
 
 function GetApp() {
   return (
-    <div>GetApp</div>
+    <section className="flexCenter w-full flex-xol pb-[100px]">
+      <div className="get-app">
+<div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+  <h2 className="bold-40 lg:border-l-red-600 xl:max-w-[320px]">Get for</h2>
+
+
+</div>
+
+      </div>
+
+
+
+    </section>
   )
 }
 
